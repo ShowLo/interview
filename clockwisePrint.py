e8e1354a2d7b45cac7143238fc5215a97426446b
ShowLo <945519267@qq.com>
2017-05-15 22:28:05 +0800

    a question met in a written examination

---
+++ clockwisePrint.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13
+
+@author: 陈佳榕
+
+某个数字建筑领域公司的笔试题目
+要求实现顺时针打印矩阵
+比如对于矩阵
+1  2  3  4
+5  6  7  8
+9  10 11 12
+13 14 15 16
+打印出1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+"""
+
+def caluteBasicLBP(matrix):
+    """
+    :param matrix:list[list],二维矩阵
+    """
+    pass
